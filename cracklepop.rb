084d784b54f3533ff6021e5304bb371b3151d379
mkeung <mikekeung@gmail.com>
2013-12-01 11:35:36 -0500

    print out 1 to 100 based on requirements given

--- cracklepop.rb
+++ cracklepop.rb
@@ -3,3 +3,14 @@
 #If it's divisible by 5, print Pop.
 #If it's divisible by both 3 and 5, print CracklePop. You can use any language.
 
+for number in 1..100
+	if (number % 3 == 0) && (number % 5 == 0)
+		puts "CracklePop"
+	elsif (number % 3 == 0)
+		puts "Crackle"
+	elsif (number % 5 == 0)
+		puts "Pop"
+	else
+		puts number
+	end
+end
