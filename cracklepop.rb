c48e6271b36147917cc7e93bec115fafec5adc74
mkeung <mikekeung@gmail.com>
2013-12-01 11:46:56 -0500

    modulate, make it more flexible

--- cracklepop.rb
+++ cracklepop.rb
@@ -3,14 +3,25 @@
 #If it's divisible by 5, print Pop.
 #If it's divisible by both 3 and 5, print CracklePop. You can use any language.
 
-for number in 1..100
-	if (number % 3 == 0) && (number % 5 == 0)
-		puts "CracklePop"
-	elsif (number % 3 == 0)
-		puts "Crackle"
-	elsif (number % 5 == 0)
-		puts "Pop"
-	else
+def crackle(number)
+	return "Crackle" if number % 3 == 0
+end
+
+def pop(number)
+	return "Pop" if number % 5 == 0
+end
+
+start_num = 1
+end_num = 100
+
+for number in start_num..end_num
+	output_msg = ""
+	output_msg += crackle(number).to_s
+	output_msg += pop(number).to_s
+
+	if output_msg.length == 0
 		puts number
+	else
+		puts output_msg
 	end
 end
